5d340448e633f22143a2641745fcbd01ee59054f
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-03-25 20:16:37 +0100

    feat: add Plane class with his methods

---
+++ src/main/java/org/lessons/java/Plane.java
@@ -0,0 +1,74 @@
+package org.lessons.java;
+
+public class Plane {
+    private String brand;
+    private String model;
+    private int power;
+    private int remainingFuel;
+
+    public Plane(String brand, String model, int power, int remainingFuel) {
+        this.brand = brand;
+        this.model = model;
+        this.power = power;
+        this.remainingFuel = remainingFuel;
+    }
+
+    public Plane(String brand, String model, int power) {
+        this.brand = brand;
+        this.model = model;
+        this.power = power;
+        this.remainingFuel = 0;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBreand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getRemainingFuel() {
+        return remainingFuel;
+    }
+
+    public void setRemainingFuel(int remainingFuel) {
+        this.remainingFuel = remainingFuel;
+    }
+
+    public void start() {
+        IO.println("This plane has been started!");
+    }
+
+    public void stop() {
+        IO.println("This plane has been stopped!");
+    }
+
+    public void addFuel(int fuelQuantity) {
+        remainingFuel += fuelQuantity;
+    }
+
+    public void fly() {
+        IO.println("This plane is flying");
+    }
+
+    public void land() {
+        IO.println("This plane is landing");
+    }
+}
